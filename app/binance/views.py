88fe570a47ae5f4251a972adffe174acae85f702
richard <richard.turra@sensedata.com.br>
2020-07-14 22:10:55 -0300

    trading macd endpoint

--- app/binance/views.py
+++ app/binance/views.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, jsonify
 from .connect import binance_connect, process_ma, get_all_symbols, query_parse_historical_data, save_symbol_data_to_mongo, process_macd
+from app.trading import buy
 import json
+from app.extensions import mongo
 
 
 binance_bp = Blueprint('binance', __name__, url_prefix='/binance')
@@ -65,3 +67,51 @@ def calculate_macd_route(symbol):
 
     r = process_macd(symbol)
     return jsonify({"macd": str(r)})
+
+@binance_bp.route('/trade')
+def trade():
+    from datetime import datetime
+    data = mongo.db["BINANCE"].find_one({"symbol":"ETCBTC"}, {"_id":0, "open_time":1, "MACD":1, "close": 1})
+    ot = data["open_time"]
+
+
+    dt = [datetime.fromtimestamp(int(val)/1000) for val in ot]
+
+    macd = data["MACD"]
+    close = data["close"]
+    stop = False
+    profit = []
+    buy = []
+    sell = []
+    for i in range(len(macd)):
+        if i!=0 and i!=len(macd)-1:
+            if macd[i] <= 0 and macd[i+1] >0:
+                # buy
+                buy.append(close[i])
+                buy_sign = True
+                buy_val = close[i]
+                print("BUY", dt[i], close[i])
+            elif macd[i] >= 0 and macd[i+1] <0 and buy_sign==True:
+                sell.append(close[i])
+                profit.append(close[i] - buy_val)
+                buy_sign = False
+                print("SELL", dt[i], close[i])
+
+    print(sum(profit))
+
+    # import plotly.graph_objects as go
+    # from datetime import datetime
+    # import plotly.express as px
+    # fig = go.Figure()
+
+    # fig.add_trace(
+    # go.Scatter(
+    #     x=dt,
+    #     y=macd
+    # ))
+
+
+    # fig.show()
+
+
+    return jsonify({"open_time": str(ot), "macd": str(macd)})
